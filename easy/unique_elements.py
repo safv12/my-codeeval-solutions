cede06d30df7da7f7fd854839f9c622e9e143d62
safv12 <ing.fran12@gmail.com>
2016-02-11 08:15:33 -0700

    Add unique_elements solution

--- easy/unique_elements.py
+++ easy/unique_elements.py
@@ -18,25 +18,34 @@ Print out the sorted list with duplicates removed, one per line.
 import sys
 
 test_cases = open(sys.argv[1], 'r')
-for line in test_cases:
 
-    unique_elements = ''
-    a = []
 
-    while len(line) >= 1:
-        element = ''
+def get_next_element(line):
+    return str(line[0:line.find(',')])
 
-        if line.find(',') == -1:
-            element = str(line[0:len(line)])
-            line = ''
+def remove_current_element(line):
+    return line[line.find(',') + 1:len(line)]
+
+
+for test_case in test_cases:
+
+    output = ''
+    unique_elements = []
+
+    while len(test_case) >= 1:
+        current_element = ''
+
+        if test_case.find(',') == -1:
+            current_element = get_next_element(test_case)
+            test_case = ''
         else:
-            element = str(test[0:line.find(',')]) + ','
-            line = line[line.find(',')+1:len(line)]
+            current_element = get_next_element(test_case)
+            test_case = remove_current_element(test_case)
 
-        if not int(element.replace(',','')) in a:
-            a.append(int(element.replace(',','')))
-            unique_elements += element
+        if not int(current_element) in unique_elements:
+            unique_elements.append(int(current_element.replace(',','')))
+            output += current_element + ','
 
-    print(unique_elements[0:-1])
+    print(output[0:-1])
 
 test_cases.close()
