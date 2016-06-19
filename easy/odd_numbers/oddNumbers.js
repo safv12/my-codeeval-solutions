ec2cd59d9a5b9ba654ed3f1ab6d65775153f3254
safv12 <ing.fran12@gmail.com>
2016-06-18 20:11:32 -0600

    Add odd numbers solution in Javascript

---
+++ easy/odd_numbers/oddNumbers.js
@@ -0,0 +1,17 @@
+/**
+ * ODD NUMBERS
+ * Print the odd numbers from 1 to 99.
+ *
+ * INPUT SAMPLE:
+ * There is no input for this program.
+ *
+ * OUTPUT SAMPLE:
+ * Print the odd numbers from 1 to 99, one number per line.
+ */
+
+var oddNumber = 1;
+
+while (oddNumber <= 99) {
+  console.log(oddNumber);
+  oddNumber += 2;
+}
